df1e109762ecab7fb5f5c12aa09cdc788843de8b
Mahek Unnisa <mahekunnisa786@gmail.com>
2023-01-24 18:44:20 +0530

    Hero section and Forms frontend

--- URLShortener/App/urls.py
+++ URLShortener/App/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index,name='Home')
+    path('',views.index,name='Home'),
+    path('login',views.login, name='Login'),
+    path('signup',views.signup, name='Signup'),
 ]
--- URLShortener/App/views.py
+++ URLShortener/App/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'App/index.html')
+
+def login(request):
+    return render(request,'App/login.html')
+
+def signup(request):
+    return render(request,'App/signup.html')
