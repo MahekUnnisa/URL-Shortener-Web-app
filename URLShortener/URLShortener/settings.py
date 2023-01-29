776afd67f294a6e4669c6f148ec639808ac6a385
Mahek Unnisa <mahekunnisa786@gmail.com>
2023-01-29 23:52:45 +0530

    Configure tailwindcss

--- URLShortener/URLShortener/settings.py
+++ URLShortener/URLShortener/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'django_browser_reload',
     # APIs
     'rest_framework',
-    # 'compressor'
+    'compressor'
 ]
 
 MIDDLEWARE = [
@@ -127,15 +127,21 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
+
+COMPRESS_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = [
-    BASE_DIR/'static'
+    os.path.join(BASE_DIR, "static"),
 ]
+
+COMPRESS_ENABLED = True
+
+STATICFILES_FINDERS = ('compressor.finders.CompressorFinder',)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-# STATIC_ROOT = "static/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
